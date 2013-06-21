2a1d0cc4149b4f32e5785ed72deeeefc50f89446
Robert Millar <rjm8306@uncw.edu>
2013-06-21 09:54:42 -0400

    Initial Commit

---
+++ AccentMark/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AccentMark
+//
+//  Created by Robert Millar on 6/21/13.
+//  Copyright (c) 2013 Robert Millar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AccentMark/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AccentMark
+//
+//  Created by Robert Millar on 6/21/13.
+//  Copyright (c) 2013 Robert Millar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
